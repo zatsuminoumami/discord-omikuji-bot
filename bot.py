a849f16e4f55915116960a4d77586ed1434e750d
zatsuminoumami <zatsuosakananano@gmail.com>
2025-04-28 22:08:33 +0900

    add:bot.py(おみくじ機能)の追加

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+import random
+import os
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+omikuji_list = ['大吉', '中吉', '小吉', '末吉', '凶']
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '!omikuji':
+        result = random.choice(omikuji_list)
+        await message.channel.send(f'🎋 おみくじ結果：{result} でした！')
+
+client.run(TOKEN)
